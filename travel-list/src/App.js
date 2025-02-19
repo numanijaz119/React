82da4a7a05cb5a0a6981e30f639e55a780b2a3b7
numanijaz119 <minibhai009@gmail.com>
2025-02-19 11:31:59 +0500

    foundation laid

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,68 @@
-export default function App(){
-  return(
-    <Logo />
-  )
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Mobile", quantity: 1, packed: true },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-function Logo(){
+function Logo() {
+  return <h1> 🌴 Far Away 👜</h1>;
+}
+
+function Form() {
   return (
-    <h1>Far Away</h1>
-  )
+    <form className="add-form">
+      <h3>What do you need for your 😊 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Items..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
