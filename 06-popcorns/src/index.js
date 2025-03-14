f789d4133fe80afdaea5ab60b44e7a2ef4557c3b
numanijaz119 <minibhai009@gmail.com>
2025-03-14 16:40:54 +0500

    react rating component added as seprate project

--- 06-popcorns/src/index.js
+++ 06-popcorns/src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      defaultRating={"3"}
-      messages={["bad", "okay", "good", "V Good", "Amazing"]}
-    />
-    <StarRating maxStar={10} color="red" size={30} />
+    <App />
   </React.StrictMode>
 );
